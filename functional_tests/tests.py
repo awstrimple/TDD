5fe2cf253332e79107ab70a9f4855e9d654a7e1f
Tony <tony.strimple@duke.edu>
2015-07-09 16:54:26 -0400

    Add boilerplate for new FT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,5 @@
 import sys
+from unittest import skip
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -123,3 +124,19 @@ class NewVisitorTest(StaticLiveServerTestCase):
             512,
             delta=5
             )
+
+    def test_cannot_add_empty_list_items(self):
+        # Edith goes to the home page and accidentally tries to submit
+        # an empty list item. She hits enter accidentally when the input
+        # box is empty.
+
+        # Home page refreshes, tells her that list items can't be blank
+
+        # Tries again, with some text, now it works
+
+        # Amazingly, accidentally enters blank item again
+
+        # Againl, she gets the error message again
+
+        # again, able to correct it by entering some text
+        self.fail("write me!")
